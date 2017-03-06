90f0918ad12b7f2a30b993a644e1f78ae7b82578
lixiangzhou <lixiangzhouno1@sina.com>
2017-03-06 15:39:12 +0800

    项目初始化

---
+++ ZZTool/ZZTool/Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZZTool
+//
+//  Created by lixiangzhou on 2017/3/6.
+//  Copyright © 2017年 lixiangzhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZZTool/ZZTool/Demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZZTool
+//
+//  Created by lixiangzhou on 2017/3/6.
+//  Copyright © 2017年 lixiangzhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
